5022018da8226fcb9aada8b4f746c22bdd045720
oysteiae <oysteiae@stud.ntnu.no>
2018-04-05 22:41:50 +0200

    øwoiehf

--- unet3d/generator.py
+++ unet3d/generator.py
@@ -140,7 +140,6 @@ def data_generator(data_file, index_list, batch_size=1, n_labels=1, labels=None,
     while True:
         x_list = list()
         y_list = list()
-        print("data_generator")
         if patch_shape:
             index_list = create_patch_index_list(orig_index_list, data_file.root.data.shape[-3:], patch_shape,
                                                  patch_overlap, patch_start_offset)
@@ -150,14 +149,12 @@ def data_generator(data_file, index_list, batch_size=1, n_labels=1, labels=None,
         if shuffle_index_list:
             shuffle(index_list)
         while len(index_list) > 0:
-            print("hallo")
             index = index_list.pop()
             add_data(x_list, y_list, data_file, index, augment=augment, augment_flip=augment_flip,
                      augment_distortion_factor=augment_distortion_factor, patch_shape=patch_shape,
                      skip_blank=skip_blank, permute=permute)
             if len(x_list) == batch_size or (len(index_list) == 0 and len(x_list) > 0):
-                print(x_list)
-                print(y_list)
+                print("Yielded")
                 yield convert_data(x_list, y_list, n_labels=n_labels, labels=labels)
                 x_list = list()
                 y_list = list()
