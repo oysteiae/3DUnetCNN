c7038a21d2b25ff847c9ccefd059553e48c6a423
oysteiae <oysteiae@stud.ntnu.no>
2018-04-05 19:36:09 +0200

    Fix

--- brats/train_isensee2017.py
+++ brats/train_isensee2017.py
@@ -50,18 +50,17 @@ config["overwrite"] = False  # If True, will previous files. If False, will use 
 def fetch_training_data_files(return_subject_ids=False):
     training_data_files = list()
     subject_ids = list()
+    
     i = 1
+    data_files = list()
     for data_file in glob.glob("/localdata/Normalized/data/*"):
-        print(data_file)
         subject_ids.append(os.path.basename(data_file))
-        data_files = list()
         data_files.append(os.path.join(data_file))
 
         i += 1
 
+    label_files = list()
     for label_file in glob.glob("/localdata/Normalized/labels/*"):
-        print(label_file)
-        label_files = list()
         # Must add both training and labels
         label_files.append(os.path.join(label_file))
 
