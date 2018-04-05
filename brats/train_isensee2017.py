c7193ee8c7628829cb1b84371730b6f91669804c
oysteiae <oysteiae@stud.ntnu.no>
2018-04-05 16:40:26 +0200

    test

--- brats/train_isensee2017.py
+++ brats/train_isensee2017.py
@@ -13,7 +13,7 @@ config["patch_shape"] = None  # switch to None to train on the whole image
 config["labels"] = (1, 2, 4)  # the label numbers on the input image
 config["n_base_filters"] = 16
 config["n_labels"] = len(config["labels"])
-config["all_modalities"] = ["t1", "t1Gd", "flair", "t2"]
+config["all_modalities"] = ["t1"]
 config["training_modalities"] = config["all_modalities"]  # change this if you want to only use some of the modalities
 config["nb_channels"] = len(config["training_modalities"])
 if "patch_shape" in config and config["patch_shape"] is not None:
@@ -46,15 +46,29 @@ config["validation_file"] = os.path.abspath("isensee_validation_ids.pkl")
 config["overwrite"] = False  # If True, will previous files. If False, will use previously written files.
 
 
+
 def fetch_training_data_files(return_subject_ids=False):
     training_data_files = list()
     subject_ids = list()
-    for subject_dir in glob.glob(os.path.join(os.path.dirname(__file__), "data", "preprocessed", "*", "*")):
-        subject_ids.append(os.path.basename(subject_dir))
+    i = 1
+    for data_file in glob.glob("/localdata/Normalized/data/*"):
+        subject_ids.append(os.path.basename(i))
+        data_files = list()
+        data_files.append(os.path.join(data_file))
+
+        i += 1
+
+    for label_file in glob.glob("/localdata/Normalized/labels/*")):
+        label_files = list()
+        # Must add both training and labels
+        label_files.append(os.path.join(subject_dir))
+
+    for j in range(0, len(data_files)):
         subject_files = list()
-        for modality in config["training_modalities"] + ["truth"]:
-            subject_files.append(os.path.join(subject_dir, modality + ".nii.gz"))
+        subject_files.append(data_files[i])
+        subject_files.append(label_files[i])
         training_data_files.append(tuple(subject_files))
+
     if return_subject_ids:
         return training_data_files, subject_ids
     else:
